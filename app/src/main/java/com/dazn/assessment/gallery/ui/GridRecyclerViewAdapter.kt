2542fa5e95f6e8f744030ffb33c8fe8f354f4e09
Srikanth <3cfe8e@gmail.com>
2023-07-02 12:59:55 +0530

    added coil image loading library. able to see all images in grid.

--- app/src/main/java/com/dazn/assessment/gallery/ui/GridRecyclerViewAdapter.kt
+++ app/src/main/java/com/dazn/assessment/gallery/ui/GridRecyclerViewAdapter.kt
@@ -1,20 +1,34 @@
 package com.dazn.assessment.gallery.ui
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
+import com.dazn.assessment.gallery.R
 import com.dazn.assessment.gallery.data.model.ImageInfo
 import com.dazn.assessment.gallery.databinding.ItemGridImageBinding
 
 class GridRecyclerViewAdapter(private val images: List<ImageInfo>) :
     RecyclerView.Adapter<GridRecyclerViewAdapter.GridItemViewHolder>() {
 
+    private val TAG = "GridRecyclerViewAdapter"
+
     class GridItemViewHolder(
         private val binding: ItemGridImageBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(data: ImageInfo) {
+            Log.i("GridRecyclerViewAdapter", "bind: loading image:: ${data.title}")
             binding.sampleTxt.text = data.date
+
+            binding.gridImg.load(data.url) {
+                crossfade(true)
+                placeholder(R.drawable.baseline_downloading_24)
+                //transformations(RoundedCornersTransformation(8.0F))
+            }
         }
     }
 
